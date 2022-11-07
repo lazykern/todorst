4a43172c1a8d3c71239480e90827755c3f88a924
Phusit Somboonyingsuk <phusitsom@gmail.com>
2022-11-07 12:36:28 +0700

    Make quick add result struct attributes public

--- src/models/quick_add_result.rs
+++ src/models/quick_add_result.rs
@@ -2,9 +2,13 @@ use crate::models::task::Task;
 
 #[derive(Default, Debug, Clone, PartialEq)]
 pub struct QuickAddResult {
-    task: Option<Task>,
-    resolved_project_name: Option<String>,
-    resolved_assignee_name: Option<String>,
-    resolved_label_names: Option<Vec<String>>,
-    resolvved_section_name: Option<String>,
+    pub task: Option<Task>,
+
+    pub resolved_project_name: Option<String>,
+
+    pub resolved_assignee_name: Option<String>,
+
+    pub resolved_label_names: Option<Vec<String>>,
+
+    pub resolvved_section_name: Option<String>,
 }
