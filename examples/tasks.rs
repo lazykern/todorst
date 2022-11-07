f0732978e5861c8b13bb2d5c0273708ba34d9337
Phusit Somboonyingsuk <phusitsom@gmail.com>
2022-11-07 18:19:31 +0700

    Make example(s) use cli argument

--- examples/tasks.rs
+++ examples/tasks.rs
@@ -1,8 +1,17 @@
 use todorst::Todorst;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    token: String,
+}
+
 #[tokio::main]
 async fn main() {
-    let todorst = Todorst::new("619ed3f4ce444fede8fa73646416a9c5d1730fbb");
+    let args = Args::parse();
+    let todorst = Todorst::new(args.token.as_str());
     let tasks = todorst.get_tasks().await.unwrap();
     println!("{:?}", tasks);
 }
