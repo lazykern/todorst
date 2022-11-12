2a94ab6cb80b1de93484815251c9cb7cd9cbf06c
Phusit Somboonyingsuk <phusitsom@gmail.com>
2022-11-12 11:54:08 +0700

    Less redundancy

--- examples/projects.rs
+++ examples/projects.rs
@@ -13,7 +13,7 @@ async fn main() {
     let args = Args::parse();
     let token = args.token.as_str();
 
-    let todorst = Todorst::new(args.token.as_str());
+    let todorst = Todorst::new(token);
     let todorst_rest = todorst.rest_api();
 
     let projects = todorst_rest.get_projects().await.unwrap();
